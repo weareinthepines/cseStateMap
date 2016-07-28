ce9b3f21ca3621538715a4ab6efc167ce900f33b
Thomas English <thomaswenglish@gmail.com>
2016-07-28 13:44:12 -0400

    newestcommit

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
-<?php
-
+<?php  
+/**
+ * MyClass Class Doc Comment
+ *
+ * @category Class
+ * @package  CseStateMap
+ * @author   Tom English <tenglish@centerstarenergy.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://www.centerstarenergy.com
+ *
+ */
+header('Location: /index.html');  ?>
 ?>
 
 <!DOCTYPE html>
